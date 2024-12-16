8a2b071b2c1724a3b8ada269ae26745045b03022
David Rey <neisserrey@upeu.edu.pe>
2024-12-16 21:16:09 +0000

    feat: :sparkles: BAsic sidebar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "@nuxt/fonts",
+    "@nuxt/eslint"
+  ],
+
+  tailwindcss: {
+    exposeConfig: true,
+    editorSupport: true
+  },
+
+  colorMode: {
+    classSuffix: ""
+  },
+
+  imports: {
+    imports: [{
+      from: "tailwind-variants",
+      name: "tv"
+    }, {
+      from: "tailwind-variants",
+      name: "VariantProps",
+      type: true
+    }]
+  }
 })
